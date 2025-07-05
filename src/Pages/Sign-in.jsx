ff714cf8ec5d6b6bc69562533dda78fb2c4fadc2
WostK <wostbrock.K@ufl.edu>
2025-07-04 21:36:19 -0400

    Just added placeholder for signup/in

---
+++ src/Pages/Sign-in.jsx
@@ -0,0 +1,15 @@
+    import { useState } from 'react'
+    import '../App.css'
+    
+    
+    function Signin(){
+    return(
+        <div>
+            <div class= "box-main">
+                <h1>Placeholder for sign in/ sign up link or popup?/reset password function</h1>
+            </div>
+        </div>
+         )
+    }
+
+    export default Signin

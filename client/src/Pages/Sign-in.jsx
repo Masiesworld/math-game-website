c61c43a4729e2f5ed4ca80da8f19c3a363af91c7
WostK <wostbrock.K@ufl.edu>
2025-07-09 15:40:37 -0400

    Deleting a single line

--- client/src/Pages/Sign-in.jsx
+++ client/src/Pages/Sign-in.jsx
@@ -20,9 +20,6 @@
 
     return(
         <>
-            {/* <div class= "box-main">
-                <h1>Placeholder for Sign in/ sign up link or popup?/reset password function</h1>
-            </div> */}
         <div className= 'box-main'>
             <div className='sign-in-form'>
                 <h2>Sign In</h2>
